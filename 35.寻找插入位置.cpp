2f93ef0dfd04e816f2b90b274b9a75bd2a35783f
gamjeon <laugamjeon@icloud.com>
2017-10-28 01:00:59 +0800

    27/10/2017

---
+++ 35.寻找插入位置.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  35.寻找插入位置
+//
+//  Created by Laugamjeon on 2017/10/27.
+//  Copyright © 2017年 Laugamjeon. All rights reserved.
+//
+/*  输入一个递增数组和一个目标值，返回目标值应该在数组中的下标
+ *  遍历数组，如果遇到大于等于目标值的话，则返回下标退出，遍历完后如果没有退出，则返回数组长度。
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target) {
+    int len = (int)nums.size();
+    for(int i = 0; i < len; ++i){
+        if(nums[i] == target || nums[i] > target) return i;
+    }
+    return len;
+}
+
+int main(int argc, const char * argv[]) {
+    vector<int> arr;
+    arr.push_back(1);
+    arr.push_back(3);
+    arr.push_back(5);
+    arr.push_back(6);
+    int target = 7;
+    cout<<searchInsert(arr, target)<<endl;
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ 53.连续子数组求最大和.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  53.连续子数组求最大和
+//
+//  Created by Laugamjeon on 2017/10/28.
+//  Copyright © 2017年 Laugamjeon. All rights reserved.
+//
+/*  给出一个数组，找出其中子数组的和的最大值
+ *  这一题，即使在牛客上面做过，一开始还是没有能够做出来，这个是动态规划的入门题。首先我们定义一个最大值，一个连续数组的和，分别都为数组第一个数。然后从第二个数开始，如果前面数组的和为负数，则判断现在这个负数跟最大值的关系，如果前面数组为整数，则判断累加后的值跟最大值的关系。
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+    int max = nums[0];
+    int sum = nums[0];
+    for(int i = 1; i < nums.size(); ++i){
+        if(sum < 0){
+            sum = nums[i];
+            if(sum > max) max = sum;
+            continue;
+        }
+        sum += nums[i];
+        if(sum > max) max = sum;
+    }
+    return max;
+}
+
+
+int main(int argc, const char * argv[]) {
+    //-2,1,-3,4,-1,2,1,-5,4
+    vector<int> arr;
+    arr.push_back(-2);
+    arr.push_back(1);
+    arr.push_back(-3);
+    arr.push_back(4);
+    arr.push_back(-1);
+    arr.push_back(2);
+    arr.push_back(1);
+    arr.push_back(-5);
+    arr.push_back(4);
+    cout<<maxSubArray(arr)<<endl;
+    return 0;
+}
